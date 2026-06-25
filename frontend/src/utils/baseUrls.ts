f1ebb23db737a73f6cfdae9411df2b20dea9ce6d
Hacnine <167700896+JulfyAlNayeem@users.noreply.github.com>
2026-06-26 05:31:58 +0600

    Update baseUrls.ts

--- frontend/src/utils/baseUrls.ts
+++ frontend/src/utils/baseUrls.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = '/api/';
+export const BASE_URL = import.meta.env.VITE_API_URL || '/api/';
 export const AUTH_URL = `${BASE_URL}user/`;
+export const MEDIA_BASE_URL = import.meta.env.VITE_MEDIA_URL || '/';
